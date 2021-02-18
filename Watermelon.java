789623d8ceca13160dc0e6861504de85e19faaa7
Ferru9 <60952524+Ferru9@users.noreply.github.com>
2021-02-18 11:01:11 -0600

    Version 1

---
+++ Watermelon.java
@@ -0,0 +1,19 @@
+
+
+public class Watermelon {
+
+    private String color = "";
+    private static int count = 0; 
+    private float weight = 0;
+
+    public Watermelon(String input_color, float input_weight){
+
+        color = input_color;
+        weight = input_weight;
+        count++;
+    }
+    public String toString(){
+        return "This instance of Watermelon is " + color + " and it is " + weight + "lb. So far you have created " + count + " Watermelons.";
+       }  
+    
+}
---
+++ WatermelonDriver.java
@@ -0,0 +1,15 @@
+public class WatermelonDriver {
+    public static void main(String args[]){  
+
+        Watermelon s1=new Watermelon("red",  (float) 0.55);
+        System.out.println(s1); 
+
+        Watermelon s2=new Watermelon("blue", (float) 0.75);
+        
+
+        Watermelon s3=new Watermelon("orange", (float) 2);
+        System.out.println(s3);
+        
+      }  
+    
+}

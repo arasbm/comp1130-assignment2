d887962101e9f006acbebbd275fd3e664e441021
OffRecordAvocado <78003482+OffRecordAvocado@users.noreply.github.com>
2021-02-22 15:43:17 -0800

    Assignment 2 Iosif Mitiashin

---
+++ Banana.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Banana {
+    private String color = "";
+    private static int count = 0;
+    private float weight=0;
+    
+    public  Banana(String input_color, float input_weight){
+        color=input_color;
+        weight=input_weight;
+        count++;
+    }
+    public String toString(){
+        return "This instance of Banana is "+color+" and it's "+weight+"lb. So far you have created "+count+" Banana/s";
+    }
+    
+    
+
+    
+}
---
+++ BananaDriver.java
@@ -0,0 +1,9 @@
+public class BananaDriver {
+    public static void main(String[] args){
+        Banana banana1 = new Banana ("green", 3);
+        System.out.println(banana1.toString());
+        Banana banana2 = new Banana ("red", 2);
+        Banana banana3 = new Banana ("yellow", 4);
+        System.out.println(banana3.toString());
+    }
+}

7b2b5d7295aefa75374befd280a7409f01986082
ShivanshJoshi19 <77924846+ShivanshJoshi19@users.noreply.github.com>
2021-02-23 12:07:06 +0530

    Computer Assignment

---
+++ Black_currantdriver.java
@@ -0,0 +1,11 @@
+public class Black_currantdriver {
+    public static void main(String[] args)
+    {
+        Black_currant instance1 = new Black_currant("black", 1.200F);
+        System.out.println(instance1.toString());
+        Black_currant instance2 = new Black_currant("black", 1.500F);
+        System.out.println(instance2.toString());
+        Black_currant instance3 = new Black_currant("black", 1.700F);
+        System.out.println(instance3.toString());
+    }
+}

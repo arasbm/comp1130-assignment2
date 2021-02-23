8c232eb3862d18598e4cf231874e602f039d4efc
Bony3000 <donugha@gmail.com>
2021-02-23 13:55:33 -0800

    Create AppleDriver.java

---
+++ AppleDriver.java
@@ -0,0 +1,12 @@
+public class AppleDriver
+{
+    public static void main (String[] args)
+    {
+        Apple ap1 = new Apple ("Red", 2.25f);
+        Apple ap2 = new Apple ("Green", 1.28f);
+        Apple ap3 = new Apple ("Green", 1.90f);
+        
+        System.out.println(ap3);
+    }
+
+}
